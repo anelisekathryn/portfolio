c3e1d57746f87946ffd37b998cc73d68b2d7534c
Anelise Bergin <anelise.bergin@gmail.com>
2021-12-31 17:47:20 -0700

    footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import { HashLink } from 'react-router-hash-link';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
-    <div>
-      <p>footer links here</p>
+    <div className='footer'>
+      <div className='left-footer'>
+        <HashLink smooth to='/home#top'>home</HashLink>
+        <HashLink smooth to='/home#about'>about</HashLink>
+        <HashLink smooth to='/home#portfolio'>portfolio</HashLink>
+        <HashLink smooth to='/home#experience'>experience</HashLink>
+      </div>
+      <div className='right-footer'>
+        <HashLink smooth to='/home#connect'>connect</HashLink>
+        <p>connect icons will be here</p>
+        <Link>download resume</Link>
+      </div>  
     </div>
   )
 }
