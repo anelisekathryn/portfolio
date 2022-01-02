3a04b42a961309ac8a53a81325f09285cbcec9b1
Anelise Bergin <anelise.bergin@gmail.com>
2022-01-02 12:55:40 -0700

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,16 @@ export default function Header() {
       <div className='header-text'>
         <h1>ANELISE KATHRYN</h1>
         <h2>experienced marketing professional turned software engineer</h2>
-        <h6>+</h6>
-        <h3>creative problem solver with an eye for design and exceptional attention to detail</h3>
-        <h3>software engineer with a full stack education</h3>
-        <h3>12 years of marketing and brand management experience</h3>
-        <h3>10 years of graphic design expertise</h3>
-        <h3>8 years leadership experience</h3>
+        <div className='header-subtext'>
+          <h6>+</h6>
+          <div className='header-dropdown'>
+            <h3>creative problem solver with an eye for design and exceptional attention to detail</h3>
+            <h3>software engineer with a full stack education</h3>
+            <h3>12 years of marketing and brand management experience</h3>
+            <h3>10 years of graphic design expertise</h3>
+            <h3>8 years leadership experience</h3>
+          </div>
+        </div>
       </div>  
     </div>
   )
