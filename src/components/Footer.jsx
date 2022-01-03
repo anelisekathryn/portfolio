5a631fe1978866d515d192e2498a8d28f109f38e
Anelise Bergin <anelise.bergin@gmail.com>
2022-01-03 11:57:08 -0700

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,37 @@
 import '../assets/css/Footer.css';
 import { HashLink } from 'react-router-hash-link';
 import { Link } from 'react-router-dom';
+import email from '../assets/graphics/email-white.png'
+import github from '../assets/graphics/github-white.png'
+import linkedin from '../assets/graphics/linkedin-white.png'
 
 export default function Footer() {
   return (
     <div className='footer'>
-      <div className='left-footer'>
+      <div className='footer-left'>
         <HashLink smooth to='/home#top'>home</HashLink>
         <HashLink smooth to='/home#about'>about</HashLink>
         <HashLink smooth to='/home#portfolio'>portfolio</HashLink>
         <HashLink smooth to='/home#experience'>experience</HashLink>
       </div>
-      <div className='right-footer'>
-        <HashLink smooth to='/home#connect'>connect</HashLink>
-        <p>connect icons will be here</p>
-        <Link>download resume</Link>
+      <div className='footer-right'>
+        <div className="footer-icons">
+          <a href="mailto:anelise.bergin@gmail.com"><img alt="email icon" src={email}></img>
+          </a>
+          <a
+            href="https://github.com/anelisekathryn"
+            target="_blank"
+            rel="noreferrer"
+          ><img alt="github icon" src={github}></img>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/anelisebergin/"
+            target="_blank"
+            rel="noreferrer"
+          ><img alt="linkedin icon" src={linkedin}></img>
+          </a>
+        </div>  
+        <Link className='footer-button'>download resume</Link>
       </div>  
     </div>
   )
